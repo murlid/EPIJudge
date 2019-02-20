fd6bde966103079d319c98872f5381c86950d9ae
Murlidhar <murlidhar@mac.com>
2019-02-20 17:34:07 -0500

    Update SumRootToLeaf.java

--- epi_judge_java_solutions/epi/SumRootToLeaf.java
+++ epi_judge_java_solutions/epi/SumRootToLeaf.java
@@ -3,6 +3,17 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+/*
+  Consider a Binary Tree in which each node contains a binary digit. A root to leaf path can be associated with a binary number,
+  The MSB is at the root.
+  
+  Design an alo, to compute the sum of the binary numbers represented by the root to leaf paths
+
+ The insight here is to recognize that paths share nodes and that it is not necessary to repeat the computations across the
+ shared nodes. To compute the integer for the path from the root to any node, we take the integer for the root's parent , double it 
+ and add the bit at that node.
+
+*/
 public class SumRootToLeaf {
   @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
