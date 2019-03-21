68b402dc7e4bf26f20018d9f7424c6fcd99f5ba6
Murlidhar <murlidhar@mac.com>
2019-03-20 21:51:20 -0400

    Update BuyAndSellStock.java

--- epi_judge_java_solutions/epi/BuyAndSellStock.java
+++ epi_judge_java_solutions/epi/BuyAndSellStock.java
@@ -5,6 +5,12 @@ import epi.test_framework.GenericTest;
 
 import java.util.List;
 
+/*
+   Buy and sell a stock exactly once.
+   e.g give a sequence of stock prices - 310,315,275,295,260,270,290,230,255,250
+   max profit is: buy @ 260 and sell @290 to make a profit of 30.
+*/
+
 public class BuyAndSellStock {
 
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
@@ -25,4 +31,8 @@ public class BuyAndSellStock {
                                 new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
+  
+  /*
+    The time complexity is O(n) and space is O(1), where n is the length of the price array input
+  */
 }
